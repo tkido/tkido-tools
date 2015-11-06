23de0286c403496de3a9e7ef970686fd0b135cd1
tkido <takanorikido@gmail.com>
2015-11-06 15:15:24 +0900

    Update

--- server/main.py
+++ server/main.py
@@ -1,22 +1,18 @@
 #!/usr/local/bin/python
 # -*- coding:utf-8 -*-
 
-import utf8
 import re
+import webapp2
 
-from google.appengine.ext import webapp
-from google.appengine.api import urlfetch
 from google.appengine.api import memcache
-from google.appengine.ext.webapp.util import run_wsgi_app
-
-import tenjin
-from tenjin.helpers import escape, to_str
-from tenjin.helpers.html import text2html
-tengine = tenjin.Engine(layout="template/base.html", cache=tenjin.MemoryCacheStorage())
+from google.appengine.api import namespace_manager
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 import mylist
 import sil
 import svl
+import tengine
 
 def error_page(org, error_message):
     org.error(500)
@@ -24,29 +20,29 @@ def error_page(org, error_message):
         'page_title': 'エラー',
         'error_message' : error_message,
     }
-    org.response.out.write(tengine.render('template/error.html', context))
+    org.response.out.write(tengine.render(':error', context))
 
 #
 # トップページ
 #
-class IndexHandler(webapp.RequestHandler):
+class IndexHandler(webapp2.RequestHandler):
     def get(self):
         context = {
             'page_title' : 'ちょっとしたツール置き場',
         }
-        self.response.out.write(tengine.render('template/index.html', context))
+        self.response.out.write(tengine.render(':index', context))
 
 #
 # マイリスト変換
 #
-class MylistHandler(webapp.RequestHandler):
+class MylistHandler(webapp2.RequestHandler):
     def get(self):
         context = {
             'page_title' : 'マイリストを貼り付け用iframeに変換',
         }
-        self.response.out.write(tengine.render('template/mylist.html', context))
+        self.response.out.write(tengine.render(':mylist', context))
 
-class MylistConvertHandler(webapp.RequestHandler):
+class MylistConvertHandler(webapp2.RequestHandler):
     def post(self):
         url = self.request.get('url')
         r = re.compile("http://www\.nicovideo\.jp/(?:my/)?mylist/(?:#/)?(.*)")
@@ -65,19 +61,19 @@ class MylistConvertHandler(webapp.RequestHandler):
                     'page_title' : 'マイリスト変換結果',
                     'result' : mylist.convert(response.content),
                 }
-                self.response.out.write(tengine.render('template/mylist_convert.html', context))
+                self.response.out.write(tengine.render(':mylist_convert', context))
 
 #
 # SIL問題変換
 #
-class SILHandler(webapp.RequestHandler):
+class SILHandler(webapp2.RequestHandler):
     def get(self):
         context = {
             'page_title' : '『学辞郎』からP-Study System用SIL問題集を作成',
         }
-        self.response.out.write(tengine.render('template/sil.html', context))
+        self.response.out.write(tengine.render(':sil', context))
 
-class SILConvertHandler(webapp.RequestHandler):
+class SILConvertHandler(webapp2.RequestHandler):
     def post(self):
         result = sil.convert(self.request.get('file'),
                              self.request.get('name'),
@@ -90,14 +86,14 @@ class SILConvertHandler(webapp.RequestHandler):
 #
 # SVL問題変換
 #
-class SVLHandler(webapp.RequestHandler):
+class SVLHandler(webapp2.RequestHandler):
     def get(self):
         context = {
             'page_title' : '『英辞郎』からP-Study System用SVL問題集を作成',
         }
-        self.response.out.write(tengine.render('template/svl.html', context))
+        self.response.out.write(tengine.render(':svl', context))
 
-class SVLConvertHandler(webapp.RequestHandler):
+class SVLConvertHandler(webapp2.RequestHandler):
     def post(self):
         result = svl.convert(self.request.get('file'),
                              self.request.get('name'),
@@ -108,17 +104,13 @@ class SVLConvertHandler(webapp.RequestHandler):
         self.response.out.write(result)        
 
 
-def main():
-    application = webapp.WSGIApplication([('/', IndexHandler),
-                                          ('/mylist', MylistHandler),
-                                          ('/mylist_convert', MylistConvertHandler),
-                                          ('/sil', SILHandler),
-                                          ('/sil_convert', SILConvertHandler),
-                                          ('/svl', SVLHandler),
-                                          ('/svl_convert', SVLConvertHandler),
-                                         ],
-                                         debug=True)
-    run_wsgi_app(application)
 
-if __name__ == '__main__':
-    main()
+app = webapp2.WSGIApplication([('/', IndexHandler),
+                               ('/mylist', MylistHandler),
+                               ('/mylist_convert', MylistConvertHandler),
+                               ('/sil', SILHandler),
+                               ('/sil_convert', SILConvertHandler),
+                               ('/svl', SVLHandler),
+                               ('/svl_convert', SVLConvertHandler),
+                              ],
+                              debug=True)
